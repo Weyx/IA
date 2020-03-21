5c098bda29f441e1aaa4288cc234861ad958b9bf
Paris <parisantoine78@gmail.com>
2020-03-21 01:34:01 +0100

    EVOL first part port output (3.1 not done yet)

--- Part4/main.py
+++ Part4/main.py
@@ -2,12 +2,20 @@ import sys
 import matplotlib.pyplot as plt
 import gzip
 import numpy as np
+import time
 
 imageFile = gzip.open('./samples/train-images-idx3-ubyte.gz','r')
 imageFile.read(16)
 labelFile = gzip.open('./samples/train-labels-idx1-ubyte.gz','r')
 labelFile.read(8)
 
+# LAYER_SIZES = [3,2,1]
+IMAGE_SIZE = 28
+LAYER_SIZES = [IMAGE_SIZE * IMAGE_SIZE, 100, 10]
+
+
+
+# --- READ IMAGE INTO DATASET ---
 def ascii_show(image):
     print("\n\n")
     for y in image:
@@ -18,30 +26,79 @@ def ascii_show(image):
 
 def readNewImage():
     # READ IMAGE (28x28)
-    imageSize = 28
     nbImages = 1
 
-    buf = imageFile.read(imageSize * imageSize * nbImages)
+    buf = imageFile.read(IMAGE_SIZE * IMAGE_SIZE * nbImages)
     data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    data = data.reshape(nbImages, imageSize, imageSize, 1)
+    data = data.reshape(nbImages, IMAGE_SIZE, IMAGE_SIZE, 1)
 
     image = np.asarray(data[0]).squeeze()
-    ascii_show(image)
+    imageConcated = np.concatenate(image[0:IMAGE_SIZE])
+    # ascii_show(image)
 
     # READ LABELS (1, 2, 3, ..., 9)
     buf = labelFile.read(1)
     label = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
-    print(label)
+    # print(label)
 
     returnedValues = dict()
-    returnedValues['imageTab'] = image
+    returnedValues['imageTab'] = imageConcated
     returnedValues['label'] = label[0]
+
     return returnedValues
+
+# ---  ---
+def initWeightTab():
+    weightTab1 = np.random.rand(LAYER_SIZES[1], LAYER_SIZES[0]) / (IMAGE_SIZE * IMAGE_SIZE)
+    weightTab2 = np.random.rand(LAYER_SIZES[2], LAYER_SIZES[1]) / (IMAGE_SIZE * IMAGE_SIZE)
+
+
+    # print("nombre de dimensions de x1: ", weightTab.ndim)
+    # print("forme de weightTab: ", weightTab.shape)
+    # print("taille de weightTab: ", weightTab.size)
+    # print("type de weightTab: ", weightTab.dtype)
+
+    return [weightTab1, weightTab2]
+
+# --- Calculate potential ---
+def potOutputLayer1Calcul(weightL1, imageTab):
+    print(weightL1.size)
+    print(len(weightL1))
+    tic = time.perf_counter()
+    # pot = []
+    pot1 = []
+
+    # localResult = 0
+    # for h in range(LAYER_SIZES[1]):
+    #     localResult = 0
+    #     for j in range(LAYER_SIZES[0]):
+    #         localResult += weightL1[h][j] * imageTab[j]
+    #     pot.append(localResult)
+
+    for h in np.arange(LAYER_SIZES[1]):
+        pot1.append(np.sum(weightL1[h] * imageTab))
+
+    toc = time.perf_counter()
+    print(f"potOutput function => {toc - tic:0.4f} seconds")
+    # print(pot)
+    print(pot1)
+
+    return pot1
+
 
 if __name__ == "__main__":
     for i in range(1):
         returnedValue = readNewImage()
         # print(returnedValue.get("label"))
         # print(returnedValue.get("imageTab"))
+        label = returnedValue.get("label")
+        imageTab = returnedValue.get("imageTab") / 255
+        # print(imageTab)
+
+        weightTab = initWeightTab()
+        # print(weightTab[1].size)
+
+        potentialOutputLayer1 = potOutputLayer1Calcul(weightTab[0], imageTab)
+        # print(potentialOutputLayer1)
 
 #https://stackoverflow.com/questions/40427435/extract-images-from-idx3-ubyte-file-or-gzip-via-python => first url used to read in gz file and extract data
